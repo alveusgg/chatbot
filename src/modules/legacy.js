10ca4de13bd60167e74ac5de20582057ad370a68
SpaceVoyage <spacevoyage@alveussanctuary.org>
2024-07-19 16:48:45 -0500

    UTC time for run function

--- src/modules/legacy.js
+++ src/modules/legacy.js
@@ -2238,7 +2238,7 @@ async function setPTZRoamMode(controller, scene) {
 function runAtSpecificTimeOfDay(hour, minutes, func) {
 	const twentyFourHours = 86400000;
 	const now = new Date();
-	let eta_ms = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minutes, 0, 0).getTime() - now;
+	let eta_ms = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), hour, minutes, 0, 0).getTime() - now;
 	if (eta_ms < 0) {
 		eta_ms += twentyFourHours;
 	}
