04ee3419d1fb29189ce4f31a98ea5ba68f1270a8
Landon <landontmuhl@gmail.com>
2024-09-01 10:47:03 -0500

    fix resetcam mistake

--- src/modules/legacy.js
+++ src/modules/legacy.js
@@ -1480,7 +1480,7 @@ async function checkExtraCommand(controller, userCommand, accessProfile, channel
 		case "resetcam":
 			let camname = arg1Clean;
 			//remove possible cam wording
-			camname  = customCommandAlias[camname] || camname; // allow for cam name aliases
+			camname  = config.customCommandAlias[camname] || camname; // allow for cam name aliases
 			camname = "fullcam " + camname;
 			controller.connections.obs.local.restartSceneItem(controller.connections.obs.local.currentScene, camname);
 			break;
