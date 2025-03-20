0c3ded4b134103f3376dfee219587cd655cd7636
MattIPv4 <me@mattcowley.co.uk>
2025-03-20 14:14:11 +0000

    Directly increment next Date to avoid looping

--- src/utils/schedule.js
+++ src/utils/schedule.js
@@ -1,26 +1,40 @@
 /**
+ * Run a function at a specific Date
+ *  
+ * @param {Date} date Date to run at
+ * @param {Function} callback Function to run
+ */
+const runAtDate = (date, callback) => {
+    setTimeout(callback,  date - new Date());
+};
+
+
+/**
  * Run a function at a specific UTC time
  *  
  * @param {number} hour UTC hour
  * @param {number} minute UTC minute
  * @param {Function} callback Function to run
- * @param {boolean} loop Whether to run the function every day at the specified time
+ * @param {boolean} [loop] Whether to run the function every day at the specified time
  */
 const runAtTime = (hour, minute, callback, loop = true) => {
-    // Determine our next run time
+    // Determine our initial next run time
     const next = new Date();
     next.setUTCHours(hour);
     next.setUTCMinutes(minute);
+    next.setUTCSeconds(0);
     if (next < new Date()) next.setDate(next.getDate() + 1);
 
-    // Determine our delay
-    const delay = next - new Date();
-    setTimeout(() => {
+    // Invoke the callback and increment the next run time each day
+    const callbackLoop = () => {
         callback();
 
-        // Schedule the next run
-        if (loop) runAtTime(hour, minute, callback);
-    }, delay);
-}
+        if (loop) {
+            next.setDate(next.getDate() + 1);
+            runAtDate(next, callbackLoop);
+        }
+    };
+    runAtDate(next, callbackLoop);
+};
 
 module.exports = runAtTime;
