836f944e5bf469025245235ae3c4d8e2732bdc66
flakey5 <73616808+flakey5@users.noreply.github.com>
2025-04-02 19:01:01 -0700

    rounds & checkmark commands
    Signed-off-by: flakey5 <73616808+flakey5@users.noreply.github.com>

---
+++ src/commands/commands/showrounds.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/**
+ * @type {import('../types.d.ts').CommandRegister}
+ */
+module.exports = ({ connections: { obs } }) => {
+  return {
+    name: 'showrounds',
+    enabled: !!obs,
+    permission: {
+      group: 'operator'
+    },
+    run: async () => {
+      const hour = new Date().getUTCHours();
+
+      const sourceName = hour > 0 && hour < 12 ? 'roundsNightGraphic' : 'roundsGraphic';
+
+      await obs.local.setSceneItemEnabled('RoundsOverlay', sourceName, true);
+    }
+  }
+}
