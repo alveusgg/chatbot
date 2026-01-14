cb3ff21880ac32c7282fbddc6a32f4e1d71d3358
Niall Sharma <niall.k.sharma@proton.me>
2026-01-13 22:29:56 -0600

    added comment next to each ID specifiying the username

--- src/config/config.js
+++ src/config/config.js
@@ -33,10 +33,10 @@ const userRanks = {
 // NOTE: https://www.streamweasels.com/tools/convert-twitch-username-%20to-user-id/ Twitch username to ID conversion tool.
 let userPermissions = {
     commandPriority: ["commandAdmins", "commandSuperUsers", "commandMods", "commandOperator", "commandVips", "commandUsers"],
-    commandAdmins: ["spacevoyage", "maya", "theconnorobrien", "alveussanctuary"],
-    commandSuperUsers: ["ellaandalex", "dionysus1911", "dannydv", "maxzillajr", "illjx", "kayla_alveus", "alex_b_patrick", 
-                        "lindsay_alveus", "strickknine","tarantulizer","spiderdaynightlive","srutiloops","evantomology","amandaexpress",
-                        "coltonactually","tamarinsandjulie"],
+    commandAdmins: ["171828515" /*spacevoyage*/, "235835559" /*maya*/, "551538396" /*theconnorobrien*/ , "636587384" /*alveussanctuary*/],
+    commandSuperUsers: ["555646161" /*ellaandalex*/, "442741947" /*dionysus1911*/, "189754522" /*dannydv*/, "431062664" /*maxzillajr*/, "400034678" /*illjx*/, "814245444" /*kayla_alveus*/, "599137366" /*alex_b_patrick*/, 
+                        "936262698" /*lindsay_alveus*/, "strickknine","460302677" /*tarantulizer*/,"159561333" /*spiderdaynightlive*/,"1089847926"/*srutiloops*/,"524563217" /*evantomology*/,"132830613" /*amandaexpress*/,
+                        "204799332" /*coltonactually*/,"946749672" /*tamarinsandjulie*/],
     commandMods: [userRanks.mods],
     commandOperator: [],
     commandVips: [userRanks.vips],
